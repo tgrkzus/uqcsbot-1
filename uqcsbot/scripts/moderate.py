e6c074cec45df87390e2128a0f0b9db1f09e35ef
Tom Richardson <tgrkzus@gmail.com>
2018-03-13 01:08:47 +1000

    more setup. Need to get support for on_reaction and on_archive/on_delete of channels. As well as wait for persistent state to be added

--- uqcsbot/scripts/moderate.py
+++ uqcsbot/scripts/moderate.py
@@ -1,21 +1,30 @@
 from uqcsbot import bot, Command
 
 @bot.on_command("add_mod")
-def handle_cat(command: Command):
+def add_mod(command: Command):
 	if not command.has_arg():
 		bot.post_message(command.channel, "Invalid usage")
     args = command.args
 
-@bot.on_command("moderate")
-def handle_cat(command: Command):
+@bot.on_command("remove_mod")
+def remove_mod(command: Command):
 	if not command.has_arg():
 		bot.post_message(command.channel, "Invalid usage")
     args = command.args
+
+@bot.on_command("get_mods")
+def get_mods(command: Command):
+	pass
+
+# On archive clean up moderated channel from persistent state
+def archive_channel():
+	pass
 
 class ModeratedChannel(object):
-	def __init__(channel_id):
+	def __init__(channel_id, delete_emoji):
 		self.channel_id = channel_id
 		self.moderators = []
+		self.delete_emoji = delete_emoji; # Probably store this as :delete_emoji:
 
 	def add_moderator(user_id):
 		if user_id not in self.moderators:
@@ -29,11 +38,10 @@ class ModeratedChannel(object):
 		else:
 			return False
 
-	def set_delete_emoji():
-		pass
-
-	def get_delete_emoji():
-		pass
-
 	def try_delete(asker, target):
-		pass
+		'''
+		When the appropriate reaction is detected on a moderated channel.
+		Try delete the target message. Fails if the reactor isn't a moderator
+		'''
+		if asker in self.moderators:
+			# Delete message
